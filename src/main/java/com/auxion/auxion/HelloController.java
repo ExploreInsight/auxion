61d562543631c7743ee7d7d93800d44dd211921f
ExploreInsight <chiragchandel1016@gmail.com>
2023-11-23 19:33:02 +0530

    Login pages Created

--- src/main/java/com/auxion/auxion/HelloController.java
+++ src/main/java/com/auxion/auxion/HelloController.java
@@ -1,19 +1,45 @@
 package com.auxion.auxion;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.io.IOException;
+
+//import javax.swing.*;
+
+public class HelloController<XMLLoader> {
     @FXML
     private Label welcomeText;
     @FXML
     private Button btn;
+    @FXML
+    private Button btn1;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText(btn.getText());
-        btn.setText("Welcome to JavaFX Application!");
+    protected void onAdminClick() throws IOException {
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("AdminLogin.fxml"));
+        primaryStage.setTitle("Admin Window");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+    @FXML
+    protected void onUserClick() throws IOException {
+        Stage primaryStage1 = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("UserLogin.fxml"));
+        primaryStage1.setTitle("Admin Window");
+        primaryStage1.setScene(new Scene(root));
+        primaryStage1.show();
+
+
 
     }
 }
